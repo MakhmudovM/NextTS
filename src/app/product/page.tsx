6d8e68f5739b9b4cdca196a899bd8446d82b4b36
Muhammadzoir Mahmudov <kolen1470@gmail.com>
2024-05-29 00:09:35 +0500

    fix

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -33,27 +32,41 @@ const ProductList = () => {
   }, []);
 
   return (
-    <div>
-      <Navbar />
+    <div
+      className="grid grid-cols-3
+      w-auto gap-x-10 items-center text-center bg-[url('https://pngtree.com/freebackground/a-festive-scene-with-cheerful-people-exchanging-gifts-and-greetings-against-backdrop-of-colorful-bazaars-bustling-streets_15437998.html')]"
+    >
       {products.map((product) => (
-        <div key={product.id} className="flex flex-col items-center justify-center mt-5">
-          <Image 
-            src={product.thumbnail} 
-            alt={product.title} 
-            className="w-full h-80 bg-slate-500 rounded-lg object-cover"
-            width={320} 
-            height={320} 
+        <div
+          key={product.id}
+          className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4"
+        >
+          <Image
+            src={product.thumbnail}
+            alt={product.title}
+            className="rounded-t-lg"
+            width={300}
+            height={300}
           />
-          <h1 className="text-4xl mb-5 mt-3">{product.title}</h1>
-          <div className="flex mb-4 gap-5 font-bold">
-            <h2 className="text-3xl">Price: ${product.price}</h2>
-            <h2 className="text-3xl">Rating:✨{product.rating}</h2>
+          <div className="p-5">
+            <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {product.title}
+            </h1>
+            <span className="flex justify-between p-5 rounded-md pt-3 ">
+              <span className="flex font-bold">
+                price:
+                <h2 className=" font-bold">${product.price}</h2>
+              </span>
+              <span className="flex font-bold">
+                rating:
+                <h3 className="font-bold">{product.rating}⭐</h3>
+              </span>
+            </span>
           </div>
-          <p className="text-2xl mb-3 text-center">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores, corporis ducimus hic id quisquam tenetur? Porro quaerat eius in veniam hic soluta accusamus quia repellendus saepe. Aperiam beatae eius et doloribus sapiente vitae eligendi quae, magni eveniet iusto ex assumenda, distinctio consequuntur facere minima est? Laborum recusandae delectus accusantium voluptate.
-          </p>
-          <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4">
-            <Link href="/">Back to home</Link>
+          <button
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
+          >
+            <Link href={`./product${product.id}`}>More</Link>
           </button>
         </div>
       ))}
